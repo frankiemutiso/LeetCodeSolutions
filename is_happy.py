f33b5494ac22c4c2dfe5cc7e365357a8082a3cfd
Francis Mutiso <franckmutiso@gmail.com>
2023-02-13 16:20:41 +0300

    submit solution for 'isHappy' leetcode problem

---
+++ is_happy.py
@@ -0,0 +1,21 @@
+class Solution:
+    def __init__(self):
+        self.unique_nums = []
+
+    def isHappy(self, num: int) -> bool:
+        square_sum = 0
+
+        for i in str(num):
+            square_sum += int(i) ** 2
+
+        if num in self.unique_nums:
+            print(self.unique_nums)
+            self.unique_nums = []
+            return False
+
+        if square_sum == 1:
+            self.unique_nums = []
+            return True
+        else:
+            self.unique_nums.append(num)
+            return self.isHappy(square_sum)
